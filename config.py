b4e1b6b9f6052170b94de9a150fda889d89a4cbe
root <root@ip-172-31-21-118.ap-northeast-1.compute.internal>
2017-12-26 12:00:49 +0000

    twitter data collection

---
+++ config.py
@@ -0,0 +1,5 @@
+consumer_key = 'aohXIEuaKjjUi2bC2rYf2VaY1'
+consumer_secret = 'iJMEFKDuXmiT9ex80IGhYtz1ReFzlxMVLUsBKxWLLBgyzQKADm'
+access_token = '945150819371851776-NPDt84dZP3r9eNaiXXdpjg86FU8hKra'
+access_secret = 'vwvZPUtMRY24cZxlD4LaLYqTmkkw3nUNSoZWRKNN47ADy'
+
